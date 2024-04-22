48cb54700a0cd94d83f58c56d59491c420937870
tvisha9 <74127814+tvisha9@users.noreply.github.com>
2024-04-22 12:16:56 -0700

    tried to setitme

--- Assets/form.js
+++ Assets/form.js
@@ -1,37 +1,50 @@
 //get the form elements
-const usernameInput = document.querySelector('username');
-const titleInput = document.querySelector('title');
-const contentInput = document.querySelector('content');
-const submitButton = document.querySelector('submit');
+const usernameInput = document.getElementById('#username');
+const titleInput = document.getElementById('#title');
+const contentInput = document.querySelector('#content');
+const submitButton = document.querySelector('#submit');
 
-function saveForm() {
-    // saving data from form as an object
-    const saveFormInfo = {
-    usernameInput: usernameInput.value,
+// save all the value in one object
+const saveForm={
+    usernameInput:  usernameInput.value,
     titleInput: titleInput.value,
     contentInput: contentInput.value.trim(),
-    
-};
-// store the form data in local storage
-localStorage.setItem('formInfo', JSON.stringify(saveFormInfo));
-
 }
 
-function renderLastForm() {
-    const formInfo= JSON.parse(localStorage.getItem('saveFormInfo'));
+localStorage.setItem('saveform',JSON.stringify(saveForm));
+
+
+
+
+
+// function saveForm() {
+//     // saving data from form as an object
+//     const saveFormInfo = {
+//     usernameInput: usernameInput.value,
+//     titleInput: titleInput.value,
+//     contentInput: contentInput.value.trim(),
     
-}
+// };
+// // store the form data in local storage
+// localStorage.setItem('saveFormInfo', JSON.stringify(saveFormInfo));
+
+// }
+
+
+//     const formInfo1= JSON.parse(localStorage.getItem('saveFormInfo'));
+//     console.log(formInfo1);
+    
+
 
 submitButton.addEventListener ('click', function (event){
     event.preventDefault();
-    saveForm();
-    renderLastForm();
+   
 });
 
-function init() {
-    renderLastForm();
-}
-init();
+// // function init() {
+// //     renderLastForm();
+// // }
+// // init();
 
 
 
