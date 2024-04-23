b5214e48c5fb44c2a3ad2103f45207beaf5adcad
tvisha9 <74127814+tvisha9@users.noreply.github.com>
2024-04-22 23:38:06 -0700

    fixed the submit button eventlistner

--- Assets/form.js
+++ Assets/form.js
@@ -1,34 +1,54 @@
 //get the form elements
-const usernameInput = document.getElementById('#username');
-const titleInput = document.getElementById('#title');
-const contentInput = document.querySelector('#content');
-const submitButton = document.querySelector('#submit');
+
+const usernameInput = document.getElementById('username');
+const titleInput = document.getElementById('title');
+const contentInput = document.getElementById('content');
+const submitButton = document.getElementById('submit');
 
 // save all the value in one object
-const saveForm={
-    usernameInput:  usernameInput.value,
-    titleInput: titleInput.value,
-    contentInput: contentInput.value.trim(),
-}
 
-localStorage.setItem('saveform',JSON.stringify(saveForm));
+// const saveForm={
+//     usernameInput:  usernameInput.value,
+//     titleInput: titleInput.value,
+//     contentInput: contentInput.value,
+
+// }
+
+// console.log (saveForm);
 
 
 
+submitButton.addEventListener ('click', function (event){
+    // saveForm.usernameInput= usernameInput.value;
 
+    event.preventDefault();
+    // saveForm();
+    // clearForm();
+    const saveFormInfo = {
+        usernameInput: usernameInput.value,
+        titleInput: titleInput.value,
+        contentInput: contentInput.value,
+        
+    };
+    localStorage.setItem('saveform',JSON.stringify(saveFormInfo));
+    clearForm();
+});
 
 // function saveForm() {
 //     // saving data from form as an object
-//     const saveFormInfo = {
-//     usernameInput: usernameInput.value,
-//     titleInput: titleInput.value,
-//     contentInput: contentInput.value.trim(),
-    
-// };
-// // store the form data in local storage
-// localStorage.setItem('saveFormInfo', JSON.stringify(saveFormInfo));
+  
+// // alert("hello");
+// // // store the form data in local storage
+// // localStorage.setItem('saveFormInfo', JSON.stringify(saveFormInfo));
+// return saveFormInfo;
 
 // }
+function clearForm (){
+usernameInput.value ="";
+titleInput.value="";
+contentInput.value="";
+
+}
 
 
 //     const formInfo1= JSON.parse(localStorage.getItem('saveFormInfo'));
@@ -36,10 +56,7 @@ localStorage.setItem('saveform',JSON.stringify(saveForm));
     
 
 
-submitButton.addEventListener ('click', function (event){
-    event.preventDefault();
-   
-});
+
 
 // // function init() {
 // //     renderLastForm();
