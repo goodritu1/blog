cdd2f0b89fda396965f1a8982d08e79ba7feab21
tvisha9 <74127814+tvisha9@users.noreply.github.com>
2024-04-29 18:21:49 -0700

    finished making border boxes for the blog entry

--- Assets/blog.js
+++ Assets/blog.js
@@ -4,8 +4,27 @@ const mainEl = document.querySelector("main");
 let blogPost = JSON.parse(localStorage.getItem('formInfo'));
 
 console.log (blogPost);
+const themeSwitcher = document.querySelector('#theme-switcher');
+const container = document.querySelector('.container');
 
-// let contentEl = document.getElementById("content");
+// Set default mode to light
+let mode = 'light';
+
+// Listen for a click event on toggle switch
+themeSwitcher.addEventListener('click', function () {
+  // If mode is dark, apply light background
+  if (mode === 'dark') {
+    mode = 'light';
+    container.setAttribute('class', 'light');
+  }
+  // If mode is light, apply dark background
+  else {
+    mode = 'dark';
+    container.setAttribute('class', 'dark');
+  }
+});
+
+// let contentEl = document.getElementById("content");   
 // let titleEl = document.getElementById('title');
 // let usernameInputEl = document.getElementById('username');
 
@@ -15,6 +34,7 @@ for(i =0; i<blogPost.length; i++) {
     const blockQuoteEl= document.createElement("blockquote");
     const paraEl = document.createElement("p");
     console.log(blogPost[i]);
+    divEl.setAttribute("style", "border: 2px solid black; padding: 4px; margin-top: 2px");
     blockQuoteEl.innerHTML = blogPost[i].contentInput;
     h2El.innerHTML = blogPost[i].titleInput;
     paraEl.innerHTML=blogPost[i].usernameInput;
@@ -24,4 +44,6 @@ for(i =0; i<blogPost.length; i++) {
     mainEl.append(divEl);
 
 }
+
+
 
